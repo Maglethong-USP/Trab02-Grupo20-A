99f137737bf2ad709c61cf5ca764d2e2ec20f9f4
Maglethong Spirr <maglethongspirr@gmail.com>
2015-11-14 06:43:32 -0200

    Updating script for experiments

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-max_threads=10
 times_to_execute=10
 
 # Saving script start time
+echo "Starting Experiment at: " >> execTime
 date >> execTime
+echo "==================================================" >> execTime
 
-# Compile program
+# Compile sequential and parallel versions
 make
 
 # Remove any previous out directory
@@ -14,20 +15,48 @@ rm -r ./out
 # Create output directory
 mkdir ./out
 
-# Run for each matrix matix
-for j in 250 500 1000 1500 2000 3000 4000
+# Run for each Image in sequential
+echo "Starting Sequential Experiments: " >> execTime
+for inFile in ExtraLarge(16384x8192).pgm Large(8192x4096).pgm Medium(4096x2048).pgm Small(2048x1024).ppm ExtraLarge(16384x8192).ppm Large(8192x4096).ppm Medium(4096x2048).ppm Small(2048x1024).pgm
 do
-
 	for i in $(seq 1 $max_threads)
 	do
-		echo "Executing for $j Matix - $i Threads"
-		in_file_name="./doc/inputs/"$j".txt"
-		out_file_name="./out/"$j"-"$i"t.txt"
-		echo "./bin/prog $in_file_name $i $times_to_execute $out_file_name"
-		./bin/prog $in_file_name $i $times_to_execute $out_file_name
+		in_file_name="../../Images/"$inFile
+		out_file_name="./out/trash"
+
+		start=$(date +%s.%N)
+
+	#	./bin/sequencial $in_file_name $out_file_name
+
+		dur=$(echo "$(date +%s.%N) - $start" | bc)
+
+		message="Using image "$execTime" experiment #"$i" took "$dur
+		echo $message >> execTime
 	done
 done
 
+# Run for each Image in sequential
+echo "Starting Sequential Experiments: " >> execTime
+for inFile in ExtraLarge(16384x8192).pgm Large(8192x4096).pgm Medium(4096x2048).pgm Small(2048x1024).ppm ExtraLarge(16384x8192).ppm Large(8192x4096).ppm Medium(4096x2048).ppm Small(2048x1024).pgm
+do
+	for i in $(seq 1 $max_threads)
+	do
+		in_file_name="../../Images/"$inFile
+		out_file_name="./out/trash"
+
+		start=$(date +%s.%N)
+
+	#	./bin/parallel $in_file_name $out_file_name 2 2 -np 4 -hostfile ../../hosts
+
+		dur=$(echo "$(date +%s.%N) - $start" | bc)
+
+		message="Using image "$execTime" experiment #"$i" took "$dur
+		echo $message >> execTime
+	done
+done
+
+echo "Ended Experiment at: " >> execTime
 date >> execTime
 
-echo " ------------------------------ " >> execTime
+
+
